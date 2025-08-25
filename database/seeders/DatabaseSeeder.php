a6c3f5e79a2102ec13017ea6d33185c6fec1958e
3dward22 <159800005+3dward22@users.noreply.github.com>
2025-08-25 21:30:09 +0800

    updated createtable for smoother sht

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Admin account
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // login: password
+            'role' => 'admin',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Nurse account
+        User::create([
+            'name' => 'Nurse User',
+            'email' => 'nurse@example.com',
+            'password' => Hash::make('password'), // login: password
+            'role' => 'nurse',
+        ]);
+
+        // Student account
+        User::create([
+            'name' => 'Student User',
+            'email' => 'student@example.com',
+            'password' => Hash::make('password'), // login: password
+            'role' => 'student',
         ]);
     }
 }
